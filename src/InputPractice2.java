78bb7f615f085a2421eb83903070a0d580932c39
HarrisonVelascoLHS <100148556@lubbockisd.net>
2023-09-14 09:47:57 -0500

    Stuff

--- src/InputPractice2.java
+++ src/InputPractice2.java
@@ -1,5 +1,24 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+//NAME - Harrison Velasco
+//DATE - 9/14/23
+//PURPOSE - Learn how to define, input, and print some variables and their values.
+
+import java.util.Scanner;
+public class InputPractice2
+{
+    public static void main (String[] args)
+    {
+        Scanner keyboard = new Scanner(System.in);
+
+        String firstName;
+        String lastName;
+        System.out.println("Enter your first name :: ");
+        firstName = keyboard.next();
+        System.out.println("Enter your last name ::");
+        lastName = keyboard.next();
+
+        System.out.println("####################\n####################");
+        System.out.println(firstName + " " + lastName);
+        System.out.println("####################\n####################");
+        System.out.println(lastName + ", " + firstName);
     }
 }
